629d7c266d769074c683e1927a31eda78d403350
Sahir Nambiar <sahirnambiar@gmail.com>
2018-11-07 17:18:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+   return "There is nobody waiting to be served!"; 
+  }
+  else {
+    var nextup = katzDeliLine[0];
+        katzDeliLine.shift();
+      return `Currently serving ` + nextup + `.`;
+  }
+}
+
+function currentLine(kdl) {
+  var customerstr = "";
+  if(kdl.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    for(var i = 0; i<kdl.length; i++) {
+      var nom = kdl[i];
+      var ord = i + 1;
+      if (i === kdl.length - 1) {
+        customerstr += (`${ord}. ${nom}`);
+      }
+      else {
+        customerstr += (`${ord}. ${nom}, `);
+      }
+    }
+  }
+  return "The line is currently: " + customerstr;
+}
