6e8d0aa29cd2c38042ccca76fbfae15f907fef6b
Sahir Nambiar <sahirnambiar@gmail.com>
2018-11-07 22:05:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var katzDeliLine = [];
 var i = 0;
+
+function tellMeWhere(katzDeliLine, name) {
+  for(var i = 0; i<kdl.length; i++) {
+    kdl = kdl.toUpperCase
+name = name.toUpperCase
+return `You are number ${i+1} in line.`
+}
+if(!kdl.includes(name)) {
+  return `You aren't in line`
+}
+  }
+  
 function takeANumber(katzDeliLine) {
   katzDeliLine.push(i+1);
   i++;
@@ -16,21 +28,14 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(kdl) {
-  var custr = "";
+  var custr = [];
   if(!kdl.length) {
     return "The line is currently empty.";
   }
   else {
     for(var i = 0; i<kdl.length; i++) {
-      var nom = kdl[i];
-      var ord = i + 1;
-      if (i === kdl.length - 1) {
-        custr += (`${ord}. ${nom}`);
-      }
-      else {
-        custr += (`${ord}. ${nom}, `);
-      }
+     custr.push(`${i+1}. ${kdl[i]}`)
     }
   }
-  return "The line is currently: " + custr;
+  return "The line is currently: " + custr.join(', ');
 }
