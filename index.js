bff0f2a898367aa6d093f55dfc667ce35118bd13
Sahir Nambiar <sahirnambiar@gmail.com>
2018-11-07 21:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function tellWhere(katzDeliLine, name) {
-  for(i = 0; i<katzDeliLine.length; i++) {
-    if (katzDeliLine[i].toUpperCase() === name.toUpperCase()) {
-      return `You are number ${i+1} in line.`;
-    }
-    if (!katzDeliLine.includes(name)) {
-      return 'You don\'t seem to have taken a number.';
-    }
-  }
-}
-
 function nowServing(katzDeliLine) {
   if (!katzDeliLine.length) {
    return "There is nobody waiting to be served!"; 
