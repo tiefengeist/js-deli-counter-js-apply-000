5cb51d8e4e85bb2322afa3f256e4e26cfb31afd0
Sahir Nambiar <sahirnambiar@gmail.com>
2018-11-07 21:44:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = []
 function takeANumber(katzDeliLine) {
   katzDeliLine.push('element');
   return `Welcome. You are number ${katzDeliLine.length} in line.`;
-}}
+}
 
 function nowServing(katzDeliLine) {
   if (!katzDeliLine.length) {
